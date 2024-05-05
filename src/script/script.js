56feb0b1644ab6dc54e26e347bea0dfcb79341a3
YuriLopess <costalopesyuri@gmail.com>
2024-05-05 07:54:46 -0300

    Redoing the main structures.

--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,59 @@
+const textSlides = document.querySelectorAll(".text--slides h1");
+const slides = document.querySelectorAll(".img--slides img");
+let slideIndex = 0;
+let intervalId = null;
 
-/*=============== SWIPER ===============*/
-let swiperImages = new Swiper('.home__swiper', {
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
-    loop: true,
+function initializeSlider(){
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        textSlides[slideIndex].classList.add("displaySlide");
+    }
+    startSlideShow(); // Inicia o slideshow automaticamente ao carregar a página
+}
 
-    pagination: {
-      el: '.swiper-pagination',
-    },
+function showSlide(index){
+    if(index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
+    textSlides.forEach(textSlide => {
+        textSlide.classList.remove("displaySlide");
+    });
+    textSlides[slideIndex].classList.add("displaySlide");
+}
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+function prevSlide(){
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    showSlide(slideIndex);
+}
+
+function startSlideShow() {
+    intervalId = setInterval(nextSlide, 2000); // Altera o slide a cada 2 segundos
+}
+
+function stopSlideShow() {
+    clearInterval(intervalId); // Para o autoplay
+}
+
+// Inicia o slideshow automaticamente ao passar o mouse sobre o slider
+document.querySelector(".slider").addEventListener("mouseenter", stopSlideShow);
+
+// Continua o slideshow automaticamente ao tirar o mouse do slider
+document.querySelector(".slider").addEventListener("mouseleave", startSlideShow);
